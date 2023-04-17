d67e2ebce001fde7b8a841e243df1d10f556dc66
Chris Smith <git@smithc.dev>
2023-04-17 19:51:22 -0400

    tests complete

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -3,12 +3,14 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let x = 2;
+        let y = 2;
+        assert_eq!(x, y, "testing values x: {} and y: {}", x, y);
     }
 }
