658b64800f3179d4ff41526486740925860c2f41
Chris Smith <git@smithc.dev>
2023-04-22 19:52:01 -0400

    threads complete

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,12 +1,12 @@
 // threads3.rs
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 struct Queue {
     length: u32,
@@ -28,11 +28,13 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    let tx1 = tx.clone();
+    let tx2 = tx.clone();
 
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -40,13 +42,14 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 }
 
 fn main() {
+    let start = Instant::now();
     let (tx, rx) = mpsc::channel();
     let queue = Queue::new();
     let queue_length = queue.length;
@@ -60,5 +63,6 @@ fn main() {
     }
 
     println!("total numbers received: {}", total_received);
+    println!("total duration: {}", start.elapsed().as_millis());
     assert_eq!(total_received, queue_length)
 }
